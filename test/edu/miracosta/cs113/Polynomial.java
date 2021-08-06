c4973fea1256ea75c7cf8e6d1025cba6e36bb63e
abdalla-atalla <60201830+abdalla-atalla@users.noreply.github.com>
2021-08-05 17:41:34 -0700

    finished hw 3

--- test/edu/miracosta/cs113/Polynomial.java
+++ test/edu/miracosta/cs113/Polynomial.java
@@ -1,9 +1,133 @@
 package edu.miracosta.cs113;
 
-public class Polynomial {
+import java.util.LinkedList;
 
+public class Polynomial {
+	private LinkedList<Term> listOfTerms; // Terms List
+
+	// Constructors
 	public Polynomial() {
-		// TODO Auto-generated constructor stub
+		this.listOfTerms = new LinkedList<Term>();
+	}
+
+	// Terms List #2 aka the copy
+	public Polynomial(Polynomial other) {
+		listOfTerms = new LinkedList<Term>();
+		for (int i = 0; i < other.getNumTerms(); i++) {
+			this.addTerm(new Term(other.getTerm(i)));
+		}
+
+	}
+
+	// Getters
+	public int getNumTerms() {
+		return listOfTerms.size();
+	}
+
+	public Term getTerm(int i) {
+		return listOfTerms.get(i);
+	}
+
+	public LinkedList<Term> getPolyList() {
+		return listOfTerms;
+	}
+
+	// adds a term
+	public void addTerm(Term term) {
+
+		// case 1: there is nothing, just add to the beginning
+		if (listOfTerms.isEmpty()) {
+			listOfTerms.offerFirst(term);
+		}
+		// case 2: its not empty, now add
+		else {
+			for (int i = 0; i < this.listOfTerms.size(); i++) {
+				// check if they are similar, if yes they are then these are like terms and
+				// continue
+				if (listOfTerms.get(i).getExponent() == term.getExponent()) {
+					// if these coefficients add up to 0 remove them bc they will cancel
+					if (listOfTerms.get(i).getCoefficient() + term.getCoefficient() == 0) {
+						listOfTerms.remove(i);
+					}
+
+					// if it makes it to here then add the coefficients & set the new sum of
+					// coefficients
+					else {
+						int sumCoefficients = listOfTerms.get(i).getCoefficient() + term.getCoefficient();
+						this.listOfTerms.get(i).setCoefficient(sumCoefficients);
+					}
+					// finished here now break to go to next case
+					break;
+				}
+
+				// case 3: the List only has one term, compare the exponents and if the new term
+				// is greater put it before the term that was already present
+				else if (listOfTerms.size() == 1) {
+					if (listOfTerms.get(0).getExponent() < term.getExponent()) {
+						listOfTerms.offerFirst(term);
+						break;
+					} // if its is less than then put it in the end/ after the term that is already
+						// present
+					else {
+						listOfTerms.offerLast(term);
+						break;
+					}
+				}
+
+				//case 4: the exponents aren't equal so just add the term
+				else if (i == (this.listOfTerms.size() - 1)) {
+					listOfTerms.add(term);
+					break; 
+				} else if (listOfTerms.get(i + 1).getExponent() < term.getExponent()) {
+					listOfTerms.add(i + 1, term);
+					break; 
+
+				}
+			}
+		}
+
+	}
+
+	public void add(Polynomial other) {
+		// Add each term from Other to the polynomial
+		for (int i = 0; i < other.listOfTerms.size(); i++) {
+			this.addTerm(other.getTerm(i));
+		}
+	}
+
+	public void clear() {
+		listOfTerms.clear();
+	}
+
+	public void remove(int index) {
+		listOfTerms.remove(index);
+	}
+
+	@Override
+	public String toString() {
+		String list = "";
+		if (this.listOfTerms == null) {
+			return "null";
+		}
+		if (this.listOfTerms.isEmpty()) {
+			return "0";
+		}
+
+		// if the first term is positive
+		if (this.listOfTerms.get(0).getCoefficient() > 0) {
+			list += this.listOfTerms.get(0).toString().substring(1);
+		}
+
+		// then the first term is negative
+		else {
+			list += this.listOfTerms.get(0).toString();
+		}
+
+		// then puts the rest of the polynomial to string
+		for (int i = 1; i < listOfTerms.size(); i++) {
+			list += this.listOfTerms.get(i);
+		}
+		return list;
 	}
 
 }
--- test/edu/miracosta/cs113/Term.java
+++ test/edu/miracosta/cs113/Term.java
@@ -1,9 +1,161 @@
 package edu.miracosta.cs113;
 
-public class Term {
+public class Term implements Cloneable, Comparable<Term> {
+	private int coefficient, exponent;
+	private final static char VARIABLE_SYMBOL = 'x';
+	private final static char EXPONENT_SYMBOL = '^';
+	private final static char PLUS_SYMBOL = '+';
+	private final static char MINUS_SYMBOL = '-';
 
+	// Default Constructor
 	public Term() {
-		// TODO Auto-generated constructor stub
+		coefficient = 1;
+		exponent = 1;
+	}
+
+	// Full Constructor
+	public Term(int coefficient, int exponent) {
+		this.coefficient = coefficient;
+		this.exponent = exponent;
+	}
+
+	// Copy Constructor
+	public Term(Term other) {
+		this.coefficient = other.coefficient;
+		this.exponent = other.exponent;
+	}
+
+	// String Constructor
+	public Term(String term) {
+
+		if (!term.isEmpty()) {
+
+			// Looks for X Variable
+			if (term.contains(Character.toString(VARIABLE_SYMBOL))) {
+				// Separates Coefficient and Exponent 
+				String[] splitTerm = term.split(Character.toString(VARIABLE_SYMBOL));
+				this.coefficient = parseCoefficient(splitTerm[0]);
+
+				// Has an exponent
+				if (splitTerm.length == 2) {
+					this.exponent = parseExponent(splitTerm[1]);
+				}
+
+				// X to the first power
+				else {
+					this.exponent = 1;
+				}
+
+			}
+			// only has constant
+			else {
+				this.coefficient = parseCoefficient(term);
+				this.exponent = 0;
+			}
+		}
+		// Empty term
+		else {
+			this.coefficient = 0;
+			this.exponent = 0;
+		}
+	}
+
+
+
+	public int getCoefficient() {
+		return coefficient;
+	}
+
+	public int getExponent() {
+		return exponent;
+	}
+
+	public void setCoefficient(int coefficient) {
+		this.coefficient = coefficient;
+	}
+
+	public void setExponent(int exponent) {
+		this.exponent = exponent;
+	}
+
+	public void setAll(int coefficient, int exponent) {
+		this.coefficient = coefficient;
+		this.exponent = exponent;
+	}
+
+	
+	@Override
+	public String toString() {
+		String termString = "";
+
+		if (this.coefficient != 0) {
+			// Positive Term
+			if (this.coefficient > 0) {
+				termString += PLUS_SYMBOL;
+				if (coefficient > 1) {
+					termString += this.coefficient;
+				}
+			}
+
+			// Negative Term
+			else {
+				if (coefficient == -1) {
+					termString += MINUS_SYMBOL;
+				} else {
+					termString += this.coefficient;
+				}
+			}
+
+			if (this.exponent != 0) {
+				termString += VARIABLE_SYMBOL;
+				if (this.exponent != 1) {
+					termString += "^" + this.exponent;
+				}
+			}
+		}
+
+		return termString;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other == null) {
+			return false;
+		}
+		Term otherTerm = (Term) other;
+		return otherTerm.exponent == this.exponent && otherTerm.coefficient == this.coefficient;
+	}
+
+	public int compareTo(Term other) {
+		if (other.exponent == this.exponent) {
+			return 0;
+		} else if (other.exponent > this.exponent) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+
+
+	public Term clone() {
+		Term Copy = new Term(this.coefficient, this.exponent);
+		return Copy;
+	}
+
+	// Get exponent value from String
+	private int parseExponent(String term) {
+		return Integer.parseInt(term.substring(1));
+	}
+
+	// Get coefficient value from String
+	private int parseCoefficient(String term) {
+		if (term.length() == 1 && term.charAt(0) == MINUS_SYMBOL) {
+			return -1;
+		} else if (term.length() == 1 && term.charAt(0) == PLUS_SYMBOL) {
+			return 1;
+		} else {
+			return Integer.parseInt(term);
+		}
 	}
 
 }
